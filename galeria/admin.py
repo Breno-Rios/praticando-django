a191155b3c4271af14c70bf2b41ce3f3d99d0c23
Breno Rios <brenooliveirarios@gmail.com>
2025-08-30 19:16:09 -0300

    feat: registe class Fotografia in admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "name", "legenda")
+    list_display_links = ("id","name")
+    search_fields = ('name',)
+
+
+admin.site.register(Fotografia, ListandoFotografias)

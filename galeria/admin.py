51d9a6b15208948ede1a0515f1b9aa4f61712118
Breno Rios <brenooliveirarios@gmail.com>
2025-08-30 19:50:36 -0300

    chore: add new visualizations to the admin display

--- galeria/admin.py
+++ galeria/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "name", "legenda")
+    list_display = ("id", "name", "legenda","publicada")
     list_display_links = ("id","name")
     search_fields = ('name',)
+    list_filter = ('categoria',)
+    list_editable = ("publicada",)
+    list_per_page = 10
 
 
 admin.site.register(Fotografia, ListandoFotografias)
